a756a771c5560823431eea0870899df18b783eeb
ijasahamedstr <116961653+ijasahamedstr@users.noreply.github.com>
2026-07-12 20:23:14 +0530

    commit

--- Backend/routes/Intake.route.js
+++ Backend/routes/Intake.route.js
@@ -1,15 +1,10 @@
 import express from 'express';
-import { 
-  createIntake, 
-  deleteIntake, 
-  getAllIntakes, 
-  getSingleIntake, 
-  updateIntake 
-} from '../controller/Intake.controller.js'; // Adjust the path if your folder structure is different
+import { createIntake, deleteIntake, getAllIntakes, getSingleIntake, updateIntake } from '../controller/Intake.Controller.js';
+// Adjust the path if your folder structure is different
 
 const IntakeRouter = express.Router();
 
-IntakeRouter.post('/', createIntake);
+IntakeRouter.post('/',createIntake);
 IntakeRouter.get('/', getAllIntakes);
 IntakeRouter.get('/:identifier', getSingleIntake); // identifier can be ID or Slug
 IntakeRouter.put('/:id', updateIntake);
